94269c43f2394583d9371b0ed4443e880c5fbf5b
Brahialis0209 <rafantem@mail.ru>
2020-08-14 17:19:07 +0400

    gg

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h(j#@7sp((fmfro2%=87+zb832791=t!50icl0nqoys@#oz3=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 Debug = False
 
-ALLOWED_HOSTS = ['warm-river-17841.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['warm-river-17841.herokuapp.com', 'warm-river-17841.herokuapp.com/submit/',  '127.0.0.1']
 
 
 # Application definition
