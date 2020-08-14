9834bcc419d629b2b33ae54c29de7fd4c3c8eb66
Brahialis0209 <rafantem@mail.ru>
2020-08-14 17:36:11 +0400

    fff

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('submit/', views.submit, name='submit'),
+    path('get_pass/', views.get_pass, name='get_pass'),
 ]
